60b24ccb67307c707f38863788226f40faca0b3f
Renzo Ramos <renzoramosivan@gmail.com>
2026-05-11 21:41:24 +0200

    feat(frontend/bayas): limitar a 10 bayas por página (2 filas de 5)

--- frontend/app/controllers/BerriesController.scala
+++ frontend/app/controllers/BerriesController.scala
@@ -18,7 +18,7 @@ class BerriesController @Inject() (
     extends AbstractController(cc):
 
   private val baseUrl  = config.get[String]("pokeapi.base-url")
-  private val pageSize = 20
+  private val pageSize = 10
 
   def index(page: Int) = Action.async { implicit request =>
     val offset = (page - 1) * pageSize
