8ea1c06eba7481dced242f12e98f48ab0f9f6da0
Renzo Ramos <renzoramosivan@gmail.com>
2026-05-11 17:38:53 +0200

    feat(infraestructura): agregar decoder Circe para bayas

---
+++ src/main/scala/com/pokeapp/infrastructure/http/codec/BerryCodec.scala
@@ -0,0 +1,43 @@
+package com.pokeapp.infrastructure.http.codec
+
+import com.pokeapp.domain.model.*
+import com.pokeapp.domain.model.shared.NamedResource
+import com.pokeapp.infrastructure.http.codec.PokemonCodec.given
+import io.circe.{Decoder, HCursor}
+
+object BerryCodec:
+
+  given Decoder[BerryFlavorMap] = (c: HCursor) =>
+    for
+      potency <- c.downField("potency").as[Int]
+      flavor  <- c.downField("flavor").as[NamedResource]
+    yield BerryFlavorMap(potency, flavor)
+
+  given Decoder[Berry] = (c: HCursor) =>
+    for
+      id               <- c.downField("id").as[Int]
+      name             <- c.downField("name").as[String]
+      growthTime       <- c.downField("growth_time").as[Int]
+      maxHarvest       <- c.downField("max_harvest").as[Int]
+      naturalGiftPower <- c.downField("natural_gift_power").as[Int]
+      size             <- c.downField("size").as[Int]
+      smoothness       <- c.downField("smoothness").as[Int]
+      soilDryness      <- c.downField("soil_dryness").as[Int]
+      firmness         <- c.downField("firmness").as[NamedResource]
+      flavors          <- c.downField("flavors").as[List[BerryFlavorMap]]
+      item             <- c.downField("item").as[NamedResource]
+      naturalGiftType  <- c.downField("natural_gift_type").as[NamedResource]
+    yield Berry(
+      id,
+      name,
+      growthTime,
+      maxHarvest,
+      naturalGiftPower,
+      size,
+      smoothness,
+      soilDryness,
+      firmness,
+      flavors,
+      item,
+      naturalGiftType
+    )
